bd7c3bdd2cd025d7e12d16db2ad8e475edb23be7
kurresailakshmareddy <kurresailakshmareddy@gmail.com>
2022-11-20 11:39:27 +0000

    change

--- assets/script.js
+++ assets/script.js
@@ -16,7 +16,22 @@ if (topic === 'HTML') {
 }
 */
 
-var topics = ["HTML, CSS, Git, JavaScript"];
-for (var x = 0; x < topics.length; x++) {
-    console.log(topics[x]);
+var randomTopic = topics[Math.floor(Math.random() * topics.length)];
+var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
+
+function selectTopic() {
+ if (topics === 'HTML') {
+   console.log("Let's study HTML!");
+ } else if (topics === 'CSS') {
+   console.log("Let's study CSS!");
+ } else if (topics === 'Git') {
+   console.log("Let's study Git!");
+ } else if (topics === 'JavaScript') {
+   console.log("Let's study JavaScript!");
+ } else {
+   console.log('Please try again!');
+ }
 }
+
+listTopics();
+selectTopic();
